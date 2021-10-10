d091a5816341c6895b12459646c7589de9fa5202
Super Star <gay@sex.com>
2021-10-10 23:03:05 +0400

    remove comments

--- cp/copy.cpp
+++ cp/copy.cpp
@@ -4,31 +4,16 @@
 
 int main(int argc, char const *argv[])
 {
-	// std::cout << "count argc" << argc << "\n";
-	// std::cout << argv[0] << "\n";
-	// std::cout << argv[1] << "\n";
-	// std::cout << argv[2] << "\n";
-
 	std::string line;
 	std::ofstream out;
 	std::ifstream in(argv[1]);
 
-    if (in.is_open())
-    {
-		out.open(argv[2]);
-		if (out.is_open())
-		{
-	        while (getline(in, line))
-	        {
-	            // std::cout << line << std::endl;
-        		out << line << std::endl;
-	        }
-    	}
-    }
+	out.open(argv[2]);
+
+    while (getline(in, line))
+        out << line << std::endl;
+    
     in.close();
 
-    // std::cout << line << std::endl;
-
-	std::cout << "End of program" << std::endl;
 	return 0;
 }
