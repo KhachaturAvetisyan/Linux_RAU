328615036e3dcb21872ba1050bb224fabbea5746
Super Star <gay@sex.com>
2021-10-10 23:45:16 +0400

    correct code

--- cp/copy.cpp
+++ cp/copy.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
-#include <fstream>
-#include <string>
+#include <unistd.h>
+#include <fcntl.h>
 
 int main(int argc, char const *argv[])
 {
-	std::string line;
-	std::ofstream out;
-	std::ifstream in(argv[1]);
+	int f1 = open(argv[1], O_RDONLY);
+	int f2 = open(argv[2], O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	char buf;
 
-	out.open(argv[2]);
+	while(read(f1, &buf, 1))
+		write(f2, &buf, 1);
 
-    while (getline(in, line))
-        out << line << std::endl;
-    
-    in.close();
+	close(f1);
+	close(f2);
 
 	return 0;
 }
