5da2b2cbd1c2992a864f025930fe0b00e5ee6816
Khachatur <avetisyan.khachatur@student.rau.am>
2021-10-23 17:54:08 +0400

    fix mistakes

--- cp/copy.cpp
+++ cp/copy.cpp
@@ -5,10 +5,14 @@
 int main(int argc, char const *argv[])
 {
 	int f1 = open(argv[1], O_RDONLY);
+	if (f1 < 0)
+		std::cout << "error\n";
 	int f2 = open(argv[2], O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if (f2 < 0)
+		std::cout << "error\n";
 	char buf;
 
-	while(read(f1, &buf, 1))
+	while(read(f1, &buf, 1) > 0)
 		write(f2, &buf, 1);
 
 	close(f1);
