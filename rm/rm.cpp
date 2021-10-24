eed4a3afe2f36a9c0fbd835f6f9e1730c463b1ff
Khachatur <avetisyan.khachatur@student.rau.am>
2021-10-24 10:38:34 +0400

    rm testgit status

---
+++ rm/rm.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+int main(int argc, char const *argv[])
+{
+    if (argc < 2)
+        std::cout << "error, please enter path files\n";
+    
+    for (int i = 1; i < argc; ++i)
+    {
+        std::cout << i << " - " << argv[i] << "\n";
+        int f1 = unlink(argv[i]);
+        if (f1 < 0)
+            std::cout << "error\n";
+        // int f1 = remove(argv[i]);
+        // if (f1 < 0)
+        //     std::cout << "error\n";
+        // int dir = rmdir(argv[i]);
+        // if (dir < 0)
+        //     std::cout << "error\n";
+    }
+
+    std::cout << "end\n";
+}
