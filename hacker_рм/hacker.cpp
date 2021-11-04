b22b0297b408e761ca9f428f7dd64af19e1c7e85
Khachatur <avetisyan.khachatur@student.rau.am>
2021-11-04 12:32:06 +0400

    correct issuse

--- hacker_рм/hacker.cpp
+++ hacker_рм/hacker.cpp
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <cstring>
+#include <memory>
 
 size_t getFilesize(const char* filename) {
     struct stat st;
@@ -18,16 +19,29 @@ int main(int argc, char const *argv[])
 
     int f = open(argv[1], O_WRONLY);
     if (f < 0)
-        std::cout << "error\n";
+    {
+        fprintf(stderr, "can't open file %s, (line=%i, file=%s) \n%s\n", argv[1], __LINE__, __FILE__, strerror(errno));
+	    exit(EXIT_FAILURE);
+    }
 
-    char *buf= new char[sof];
-    memset(buf, '\0', sof);
+    auto buf = std::unique_ptr<char[]>(new char[sof]);
+    memset(buf.get(), '\0', sof);
 
-    write(f, buf, sof);
+    if (write(f, buf.get(), sof) < 0)
+		{
+			fprintf(stderr, "can't write in file %s, (line=%i, file=%s) \n%s\n", argv[1], __LINE__, __FILE__, strerror(errno));
+	   		exit(EXIT_FAILURE);
+		}
 
-    close(f);
+    if(close(f) < 0)
+	{
+	    fprintf(stderr, "can't close file %s, (line=%i, file=%s) \n%s\n", argv[1], __LINE__, __FILE__, strerror(errno));
+	    exit(EXIT_FAILURE);
+	}
 
-    delete[] buf; 
-
-    remove(argv[1]);
+    if(remove(argv[1]) < 0)
+	{
+	    fprintf(stderr, "can't remove file %s, (line=%i, file=%s) \n%s\n", argv[1], __LINE__, __FILE__, strerror(errno));
+	    exit(EXIT_FAILURE);
+	}
 }   
