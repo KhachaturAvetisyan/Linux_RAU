9f64104da0f85782d81b9d9072087d798547774b
Super Star <gay@sex.com>
2021-11-08 00:17:11 +0400

    mardavari code

--- cp/copy.cpp
+++ cp/copy.cpp
@@ -3,43 +3,39 @@
 #include <fcntl.h>
 #include <string.h>
 
+#define LOG_ERROR(error_text, error_file) print_error(error_text, error_file, __LINE__, __FILE__)
+
+void print_error(const char* error_text, const char* error_file, int line, const char* file)
+{
+    fprintf(stderr, "%s %s, (line=%i, file=%s) \n%s\n", error_text, error_file, line, file, strerror(errno));
+    exit(EXIT_FAILURE);
+}
+
 int main(int argc, char const *argv[])
 {
 	int read_fd = open(argv[1], O_RDONLY);
 	if(read_fd < 0)
-	{
-	    fprintf(stderr, "can't open file %s, (line=%i, file=%s) \n%s", argv[1], __LINE__, __FILE__, strerror(errno));
-	    exit(EXIT_FAILURE);
-	}
+		LOG_ERROR("can't open file,", argv[1]);
 
 	int write_fd = open(argv[2], O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 	if(write_fd < 0)
-	{
-	    fprintf(stderr, "can't open file %s, (line=%i, file=%s) \n%s", argv[2], __LINE__, __FILE__, strerror(errno));
-	    exit(EXIT_FAILURE);
-	}
+		LOG_ERROR("can't open file,", argv[2]);
 	
-	char buf;
+	const size_t size_buf = 4096;
+	char* buf[size_buf];
+	int read_text = -1;
 
-	while(read(read_fd, &buf, 1) > 0)
+	while(true)
 	{
-		if (write(write_fd, &buf, 1) < 0)
-		{
-			fprintf(stderr, "can't write in file %s, (line=%i, file=%s) \n%s", argv[2], __LINE__, __FILE__, strerror(errno));
-	   		exit(EXIT_FAILURE);
-		}
+		if ((read_text = read(read_fd, &buf, size_buf)) <= 0)
+			break;
+
+		if (write(write_fd, &buf, read_text) < 0)
+			LOG_ERROR("can't write file,", argv[2]);
 	}
 
 	if(close(read_fd) < 0)
-	{
-	    fprintf(stderr, "can't close file %s, (line=%i, file=%s) \n%s", argv[1], __LINE__, __FILE__, strerror(errno));
-	    exit(EXIT_FAILURE);
-	}
+		LOG_ERROR("can't, close file,", argv[1]);
 	if(close(write_fd) < 0)
-	{
-	    fprintf(stderr, "can't close file %s, (line=%i, file=%s) \n%s", argv[2], __LINE__, __FILE__, strerror(errno));
-	    exit(EXIT_FAILURE);
-	}
-
-	return 0;
+		LOG_ERROR("can't, close file,", argv[2]);
 }
