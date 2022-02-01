32d66fa6a005e8350b51929f1d8466387b9eb8f3
Khachatur <72801992+Xacho2002@users.noreply.github.com>
2022-02-01 17:24:58 +0400

    Fork/exec 2 hw (#9)
    * copy code XD
    
    * remove comments
    
    * correct code
    
    * fix mistakes
    
    * add Makefile
    
    * rm testgit status
    
    * xz
    
    * add errors
    
    * Delete Makefile
    
    * Delete rm.cpp
    
    * mardavari code
    
    * easy
    
    * GG WP
    
    * Delete main.cpp
    
    * Delete fork directory
    
    * Delete cp directory
    
    * error hendling
    
    Co-authored-by: Super Star <gay@sex.com>

---
+++ fork_2/A.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <initializer_list>
+#include <string>
+#include <vector>
+#include <iterator>
+
+#define LR(x, y) LogRecord{x, y}
+
+template <typename T>
+struct LogRecord
+{
+    std::string m_key;
+    T m_value;
+
+    LogRecord(std::string key, T value)
+    : m_key(std::move(key))
+    , m_value(value) {}
+    LogRecord() = delete;
+    LogRecord(const LogRecord&) = default;
+    LogRecord& operator=(const LogRecord&) = default;
+
+    LogRecord(LogRecord&&)  noexcept = default;
+    LogRecord& operator=(LogRecord&&)  noexcept = default;
+    ~LogRecord() = default;
+};
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, LogRecord<T> logRec)
+{
+    return out << logRec.m_key << "=" << logRec.m_value << "\n";
+}
+
+namespace detail
+{
+    template <typename T>
+    void log_fatal_helper(T&& tail)
+    {
+        std::cerr << tail << "\n";
+        std::cerr << "closing the process: " << getpid() << "\n";
+        exit(1);
+    }
+
+    template <typename T, typename ...Args>
+    void log_fatal_helper(T&& head, Args&&... args)
+    {
+        int errno_bac = errno;
+        std::cerr << head << "\n";
+        log_fatal_helper(args..., strerror(errno));
+    }
+
+}
+
+template <typename ...Args>
+void log_fatal(const std::string& errorMsg, Args&&... args)
+{
+    std::cerr << "count of log records " << sizeof...(Args) << "\n";
+    std::cerr << errorMsg << "\n";
+    (std::cerr << ... << args);
+}
+
+int main(int argc, char const *argv[])
+{
+	if (argc < 2)
+	{
+        std::cerr << "please enter printing text " << "file: " << __FILE__ << " line: "
+                  << __LINE__ << " desc: " <<  strerror(errno) << "\n";
+        exit(EXIT_FAILURE);
+	}
+
+	int child_process = fork();
+	if (child_process < 0)
+	{
+        std::cerr << "can't creat new process " << "file: " << __FILE__ << " line: "
+                  << __LINE__ << " desc: " <<  strerror(errno) << "\n";
+        exit(EXIT_FAILURE);
+	}
+	else if(child_process == 0)
+	{
+		if(execlp("./B", argv[1], NULL) < 0)
+			{
+			std::cerr << "can't run file " << "./B " << "file: " << __FILE__ << " line: "
+                  << __LINE__ << " desc: " <<  strerror(errno) << "\n";
+        	exit(EXIT_FAILURE);
+		}
+	}
+	wait(nullptr);
+}
